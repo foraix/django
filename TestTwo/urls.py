f47ae3cf652ef1965ee93f711f6d77c5a61a6435
yuantuo <362591904@qq.com>
2019-05-25 10:04:30 +0800

    简单登录

---
+++ TestTwo/urls.py
@@ -0,0 +1,11 @@
+from miaTest import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('login/', views.login),
+]
+
+
---
+++ miaTest/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+
+# 将请求定位到index.html文件中
+def index(request):
+    return render(request, 'index.html')
+
+
+# 创建url对应的函数映射
+def login(request):
+    msg = ''
+    # 判断是否是POST 返回
+    if request.method == 'POST':
+        # 接受 html name 是user和pwd的标签
+        user = request.POST.get('user', None)
+        pwd = request.POST.get('pwd', None)
+        if user == '123' and pwd == '123':
+            return redirect('https://www.baidu.com')
+        else:
+            msg = '用户名密码错'
+            return render(request, 'login.html', {'msg': msg})
+
+    # 去读index.html
+    return render(request, 'index.html', {'msg': msg})
